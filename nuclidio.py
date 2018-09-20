a407beed13c4861f3f860146d4e57b0a88a9f126
Dean Moser <demo9848@colorado.edu>
2018-09-20 14:38:26 -0600

    wrap element cards into class, add playertoken object

--- nuclidio.py
+++ nuclidio.py
@@ -5,19 +5,52 @@ import numpy as np
 SCREEN_X = 1280
 SCREEN_Y = 720
 CARD_SIZE = 64
-ISOTOPES = 10
-ELEMS = 5
 
+# INIT
 pygame.init()
 SCREEN = (SCREEN_X, SCREEN_Y)
 DISPLAY = pygame.display.set_mode(SCREEN)
+ELEMS = []
 
-CARD_GRID = np.zeros((ELEMS, ISOTOPES), np.int8)
+
+class IsotopeCard(object):
+    def __init__(self, atomic_num, isotope_num):
+        self.atomic_num = atomic_num
+        self.isotope_num = isotope_num
+
+    def draw_card(self):
+        x_coord = self.isotope_num * CARD_SIZE
+        y_coord = SCREEN_Y - ((self.atomic_num + 1) * CARD_SIZE)
+        rect = pygame.Rect(x_coord, y_coord, CARD_SIZE, CARD_SIZE)
+        pygame.draw.rect(DISPLAY, (255, 255, 255), rect, 0)
+        pygame.draw.rect(DISPLAY, (0, 0, 0), rect, 1)
+
+
+class PlayerToken(object):
+    def __init__(self):
+        self.atomic_num = 1
+        self.isotope_num = 1
+        self.draw_token()
+
+    def draw_token(self):
+        x_coord = self.isotope_num * CARD_SIZE
+        y_coord = SCREEN_Y - ((self.atomic_num + 1) * CARD_SIZE)
+        rect = pygame.Rect(x_coord, y_coord, CARD_SIZE, CARD_SIZE)
+        pygame.draw.rect(DISPLAY, (255, 63, 63), rect, 5)
+
+    def add_nuetron(self):
+        self.isotope_num += 1
+        if IsotopeCard(self.atomic_num, self.isotope_num) not in ELEMS:
+            self.isotope_num -= 1
+            self.atomic_num =+ 1
+        self.draw_token()
+
 
 # sample card locations
-## TODO: replace with csv parse for element information
-CARD_GRID[1, 1], CARD_GRID[1, 2], CARD_GRID[1, 3], CARD_GRID[1, 4] = 1, 1, 1, 1
-CARD_GRID[2, 2], CARD_GRID[2, 3], CARD_GRID[2, 4], CARD_GRID[2, 5] = 1, 1, 1, 1
+## TODO: csv parse for element information
+for i in range(1, 3):
+    for j in range(i, i + 4):
+        ELEMS.append(IsotopeCard(atomic_num=i, isotope_num=j))
 
 while True:
     for event in pygame.event.get():
@@ -26,12 +59,8 @@ while True:
             pygame.quit()
 
     # Draw cards
-    for i in range(ELEMS):
-        for j in range(ISOTOPES):
-            if CARD_GRID[i, j]:
-                rect = pygame.Rect(j * CARD_SIZE, SCREEN_Y - ((i + 1) * CARD_SIZE), CARD_SIZE, CARD_SIZE)
-                rect.inflate(-2, -2)
-                pygame.draw.rect(DISPLAY, (255, 255, 255), rect, 0)
-                pygame.draw.rect(DISPLAY, (0, 0, 0), rect, 1)
+    for elem in ELEMS:
+        elem.draw_card()
+    PlayerToken()
 
     pygame.display.flip()
